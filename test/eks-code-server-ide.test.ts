27e6bac72a49ef68d602d1f06e551f96e526eae6
Subhash Surana <51840090+subhashsurana@users.noreply.github.com>
2025-07-25 10:57:38 +0000

    Initial commit

---
+++ test/eks-code-server-ide.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EksCodeServerIde from '../lib/eks-code-server-ide-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eks-code-server-ide-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EksCodeServerIde.EksCodeServerIdeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
